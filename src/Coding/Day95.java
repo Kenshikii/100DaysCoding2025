7774850f5116f320db79ca482db2591fc6cbb6ec
Muhammad Rifky Al Fajri <envikicraft@gmail.com>
2025-12-11 22:19:44 +0800

    Create Day95.java

---
+++ src/Coding/Day95.java
@@ -0,0 +1,9 @@
+Public class Day95{
+  static void hitungLuas(int panjang,int lebar){
+        int hasil = panjang * lebar;
+        System.out.println(hasil);
+    }
+    public static void main(String[] args) {
+        hitungLuas(5, 10);
+    }
+}
